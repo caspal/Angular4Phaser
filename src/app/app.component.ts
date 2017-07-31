0bc53d44ac62d495e2afbc29a513564fce08f10c
Pascal <unknown>
2017-07-31 21:50:26 +0200

    bootstrap ui test
    Change-Id: I8c0e6b8f7080c6572f36591f1f417481eb3d3705

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { PhaserCanvasComponent } from './phaser/phaser-canvas.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+
+  @ViewChild(PhaserCanvasComponent)
+  phaserCanvasComponent: PhaserCanvasComponent;
+
+  clickCount = 0;
+  buttonEnabled = true;
+
+  onClickedOnAlbert(clicked: void) {
+    this.clickCount++;
+  }
+
+  onAlbertFinishedRoll(finsihed: void) {
+    this.buttonEnabled = true;
+  }
+
+  doBarrel(): void {
+    this.buttonEnabled = false;
+    this.phaserCanvasComponent.doBarrel();
+  }
+}
--- src/app/phaser/phaser-canvas.component.ts
+++ src/app/phaser/phaser-canvas.component.ts
@@ -1,6 +1,6 @@
 /// <reference types="phaser-ce" />
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,48 +8,67 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './phaser-canvas.component.html'
 })
 export class PhaserCanvasComponent implements OnInit {
+  @Output() private onClickedOnAlbert = new EventEmitter<void>();
+  @Output() private onAlbertFinishedRoll = new EventEmitter<void>();
+  private game: Phaser.Game;
+  private sprites: { [key: string]: Phaser.Sprite; } = {};
+  private scaleManager: Phaser.ScaleManager;
 
-  clickCount: number;
-  game: Phaser.Game;
-  sprites: { [key: string]: Phaser.Sprite; } = {};
+  constructor(private elRef: ElementRef) { }
 
-  ngOnInit(): void {
-    this.clickCount = 0;
-    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaserCanvas', { preload: this.preload, create: this.create(this) });
+  @HostListener('window:resize', ['$event'])
+  private sizeChange(event) {
+    this.scaleManager.setGameSize(this.getWidth(), 600);
+    const einsteinSprite = this.sprites.einstein;
+    einsteinSprite.x = this.game.world.centerX;
+    einsteinSprite.y = this.game.world.centerY;
   }
 
-  preload(): void {
+  ngOnInit(): void {
+    const canvasWidth: number = this.getWidth();
+    this.game = new Phaser.Game(canvasWidth, 600, Phaser.AUTO, 'phaserCanvas', { preload: this.preload, create: this.create(this) });
+    this.scaleManager = new Phaser.ScaleManager(this.game, canvasWidth, 600);
+  }
+
+  private getWidth(): number {
+    const canvasWidth: number = this.elRef.nativeElement.offsetParent.offsetWidth;
+    return canvasWidth;
+  }
+
+  private preload(): void {
     this.game.load.image('einstein', 'assets/pics/ra_einstein.png');
   }
 
-  create(canvasComp: PhaserCanvasComponent): Function {
+  private create(canvasComp: PhaserCanvasComponent): Function {
     return () => {
       const imageKey = 'einstein';
-      const sprite: Phaser.Sprite = this.game.add.sprite(400, 300, imageKey);
+      const sprite: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, imageKey);
       canvasComp.sprites[imageKey] = sprite;
       sprite.anchor.setTo(0.5, 0.5);
       sprite.inputEnabled = true;
       sprite.events.onInputDown.add(() => {
-        canvasComp.clickCount++;
+        canvasComp.onClickedOnAlbert.emit(null);
       }, this);
     };
   }
 
-  doBarrel(): void {
+  public doBarrel(): void {
     const colors: string[] = ['#9400D3', '#4B0082', '#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF0000'];
     const einsteinSprite = this.sprites.einstein;
     const style = { font: '65px Arial', fill: '#ff0044', align: 'center' };
     const text = this.game.add.text(einsteinSprite.x + 60, einsteinSprite.y + 100, 'uaahhh', style);
     text.rotation = 0.3;
     text.anchor.setTo(0.3, 0.3);
+
+    const onAlbertFinishedRoll = this.onAlbertFinishedRoll;
     const looper = setInterval(function () {
-      console.log(einsteinSprite.angle);
       einsteinSprite.angle += 10;
       text.angle += 10;
       text.addColor(colors[Math.floor(Math.random() * colors.length)], 0);
       if (einsteinSprite.angle === 0) {
         clearInterval(looper);
         text.destroy();
+        onAlbertFinishedRoll.emit(null);
       }
     }, 100);
   }
